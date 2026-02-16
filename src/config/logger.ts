f401dbc9e8b5b8ac7e0afaf9c53e20f3fc2b2481
OluwapelumiElisha <elishaoluwapelumi38@gmail.com>
2026-02-16 14:47:09 +0100

    Fixing Error

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -2,14 +2,14 @@ import winston from 'winston';
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(), // JSON for file
+  format: winston.format.json(), 
   transports: [
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     new winston.transports.File({ filename: 'logs/combined.log' }),
   ],
 });
 
-// Pretty console logs in dev
+
 if (process.env.NODE_ENV !== 'production') {
   logger.add(new winston.transports.Console({
     format: winston.format.combine(
